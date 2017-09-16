aad1329e3da5d8012ad85ffd320a9ec27661ddbf
Dmitri Babaev <dmitri.babaev@gmail.com>
2017-09-16 22:41:40 +0300

    initial commit

---
+++ setup.sh
@@ -0,0 +1,3 @@
+# http://holdem.sberbank.ai/
+# https://github.com/sberbank-ai/holdem-challenge
+pip install -e git+https://https://github.com/sberbank-ai/holdem-challenge.git/#egg=sberbank-pypokerengine&subdirectory=PyPokerEngine
---
+++ tournament.py
@@ -0,0 +1,61 @@
+import datetime
+import pandas as pd
+import multiprocessing
+from contextlib import closing
+from copy import deepcopy
+import os
+from pypokerengine.api.game import setup_config, start_poker
+from random_player import RandomPlayer
+from fish_player import FishPlayer
+from honest_player import HonestPlayer
+from fold_man import FoldMan
+# add custom bots
+from honest_agressive_bot import HonestAggressivePlayer, HonestAggressiveNumPlayersPlayer
+from preflop_lazy_bot import PreflopLazyPlayer
+
+n_jobs = 4
+num_games = 16
+
+
+def play_game(game_no):
+    config = setup_config(
+        max_round=50,
+        initial_stack=1500,
+        small_blind_amount=15,
+        summary_file=None)
+
+    players = [
+        PreflopLazyPlayer(),
+        PreflopLazyPlayer(),
+        HonestAggressivePlayer(),
+        HonestAggressiveNumPlayersPlayer(),
+        HonestPlayer(),
+        RandomPlayer(),
+        RandomPlayer(),
+        FishPlayer(),
+        FishPlayer(),
+    ]
+
+    for i, pl in enumerate(players):
+        config.register_player(name='{}. {}'.format(i, pl.__class__.__name__), algorithm=pl)
+
+    print('Starting game {}'.format(game_no))
+    game_result = start_poker(config, verbose=0)
+    result = [(p['name'], p['stack']) for p in game_result['players']]
+
+    for name, score in result:
+        print('Game #{}, {}: {}'.format(game_no, name, score))
+
+    return result
+
+
+start_time = datetime.datetime.now()
+with closing(multiprocessing.Pool(n_jobs)) as pool:
+    args = [(game_no,) for game_no in range(num_games)]
+    tasks = [pool.apply_async(play_game, arg) for arg in args]
+    game_results = [task.get(999999) for task in tasks]
+    game_scores = pd.DataFrame([pd.Series(dict(r)) for r in game_results]).sum().div(num_games)
+
+
+print('Elapsed time: {}'.format(datetime.datetime.now() - start_time))
+print(game_scores)
